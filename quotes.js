c003784010746ea8b6eb940edb9d182644d430db
ALISHBA ZAFAR <alishbazafarp108@gmail.com>
2025-02-01 17:08:29 +0500

    first commit

---
+++ quotes.js
@@ -0,0 +1,21 @@
+// fetch('http://api.quotable.io/random')
+//   .then(response => response.json())
+//   .then(data => {
+//    console.log(data.content); // Quote text
+//    console.log(data.author);  // Quote author
+//   });   
+const api_url = 'http://api.quotable.io/random';
+const  quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const btn = document.querySelector('button');
+ 
+  async function getquote (url){
+    let response = await  fetch(url)
+    let data = await response.json()
+    console.log (data)
+    quote.innerHTML=(data.content)
+    author.innerHTML=( ' —' + '  '+data.author)
+
+ }
+ getquote(api_url)
+ 
